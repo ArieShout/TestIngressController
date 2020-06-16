e1d9371481836cb7d49d8865d0fc23d515b8872f
root <root@aks-nodepool1-23923939-vmss000000.m4v2d3o4dmvuvaflkgvtagh11g.ax.internal.cloudapp.net>
2020-06-16 07:44:05 +0000

    add scripts for 06-16
    Signed-off-by: root <root@aks-nodepool1-23923939-vmss000000.m4v2d3o4dmvuvaflkgvtagh11g.ax.internal.cloudapp.net>

---
+++ 2020-06-16-default/2020-06-16-main.pod.svc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+dir="$1"
+
+mkdir -p $dir
+
+for n in `cat ips`; do
+
+	nohup ssh azureuser@${n} \
+	"wrk -t 1 -c 300 -d 600s --latency http://10.0.63.193:1025" | \
+	 tee ${dir}/log.${n}.t.1.c.300.log &
+
+done
