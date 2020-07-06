8877b7c5c90290a2143f526de26becba75cadce4
root <root@aks-nodepool1-11845990-vmss000000.5qrtnkn2oc4ezeew5ek5u2mqwf.bx.internal.cloudapp.net>
2020-07-06 06:03:20 +0000

    这里想把最大链接数改到512再做尝试
    Signed-off-by: root <root@aks-nodepool1-11845990-vmss000000.5qrtnkn2oc4ezeew5ek5u2mqwf.bx.internal.cloudapp.net>

---
+++ 2020-06-29-test-400-vs-200-by-changed-connections-number-512/2020-06-28-main.just.ingress.svc.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# 这里只是从ingress的svc发送请求，是不会走ingress相关的lb过
+# add dns record yoj-ingress-svc.zihchdomains.com -> 10.0.43.35 ClusterIP A record in dns server.
+
+set -e
+
+fold="$1"
+mkdir -p ${fold}
+
+for n in `cat ips`; do
+
+	nohup ssh azureuser@${n} \
+	"wrk -t 1 -c 300 -d 600s --latency http://yoj-ingress-svc.zihchdomains.com" | \
+	 tee ${fold}/log.${n}.t.1.c.300.log &
+
+done
